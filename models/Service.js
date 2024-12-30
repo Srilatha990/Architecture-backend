a94df9c93bb7448252ce3e2e4913cc6c83f21dac
Srilatha990 <162280646+Srilatha990@users.noreply.github.com>
2024-12-30 14:48:45 +0530

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const serviceSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Service', serviceSchema);
