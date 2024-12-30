660ecfef60059a83186a62f86b096e6edc4b9ce4
Srilatha990 <162280646+Srilatha990@users.noreply.github.com>
2024-12-30 14:19:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());  // For parsing JSON request bodies
 
 // Enable CORS for all origins (or specify a particular origin)
-app.use(cors());  // Allow all origins (for development)
+// app.use(cors());  // Allow all origins (for development)
 // app.use(cors({ origin: 'http://localhost:3000' })); // Allow only frontend origin
 
 // Connect to MongoDB
